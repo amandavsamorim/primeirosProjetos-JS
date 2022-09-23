13c01bef6bdf75417d92f92cee80235ea969f8c3
Amanda Amorim ʚĭɞ <amandavsadev@gmail.com>
2022-09-22 22:05:55 -0300

    02/06/2022

---
+++ contador/script.js
@@ -0,0 +1,11 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
